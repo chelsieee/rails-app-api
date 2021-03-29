d668227358973b0ceb91b7aca86c392a1e7e69da
Chelsie <chensifu4@gmail.com>
2021-03-29 21:35:49 +1100

    add route for blog/category

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,21 +1,43 @@
 class Api::BlogsController < ApplicationController
+
+  def index
+    render json: Blog.all
+  end
+
   def create
     puts params[:title]
     puts params[:content]
     puts params[:categoryId]
 
-    todo = Blog.create(
+    blog = Blog.create(
       title: params[:title],
       content: params[:content],
       category_id: params[:categoryId],
-      user_id: '1'
+      user_id: params[:userId]
     )
 
-    todo_valid = todo.valid?
-    if todo_valid
-      render json: {message: 'Successfully created todo list'}, status: 200
+    blog_valid = blog.valid?
+    if blog_valid
+      render json: {message: 'Successfully created blog'}, status: 200
     else
-      render json: {message: 'unable to created todo list'}, status: 400
+      render json: {message: 'unable to created blog'}, status: 400
     end
   end
+
+
+def show
+  puts params[:id]
+  render json: Blog.find(params[:id])
 end
+
+def update
+  render json: Blog.find(params[:id]).update(title: params[:title], content: params[:content], category_id: params[:categoryId], user_id: params[:userId]), status: 200
+end
+
+def destroy
+  Blog.destroy(params[:id])
+  render json: {message: 'Blog deleted successfully'}, status: 200
+end
+
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :blogs
+    resources :categories
   end
 
 
